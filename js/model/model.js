0c87fe70d3630a3657a5d989f2cc2d11f09c3353
andrii.pischanskyi <andrii.pischanskyi@ODS1-LHP-F36920.synapse.com>
2015-03-11 12:43:54 +0200

    changed prototypes implementation

--- js/model/model.js
+++ js/model/model.js
@@ -1,28 +1,19 @@
 /**
  * New node file
  */
-var Category = {
-    constructor: function(name) {
-        this.name = name;
-        return this;
-    }
+var Category = function(name) {
+    this.name = name;
 };
 
-var Preview = {
-    constructor: function(link, topic, image, shortText, category) {
-        this.link = link;
-        this.topic = topic;
-        this.image = image;
-        this.shortText = shortText;
-        this.category = category;
-        return this;
-    }
+var Preview = function(link, topic, image, shortText, category) {
+    this.link = link;
+    this.topic = topic;
+    this.image = image;
+    this.shortText = shortText;
+    this.category = category;
 };
 
-var Article = {
-    constructor: function(topic, text) {
-        this.topic = topic;
-        this.text = text;
-        return this;
-    }
+var Article = function(topic, text) {
+    this.topic = topic;
+    this.text = text;
 };
