ab5a87c7d23e17ebc474d1d134353c2e974221ec
apischan <andrew.pischanski@gmail.com>
2015-03-26 01:41:51 +0200

    try to implement tabs using angular directives

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 var wwwLinkAggregator = angular.module('wwwLinkAggregator', [
     'ngRoute',
-    'linkAggregatorControllers'
+    'linkAggregatorControllers',
+    'components'
 ]);
+
+wwwLinkAggregator.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/actual', {
+        templateUrl: 'templates/actual-item.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/completed', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/actual'
+      });
+  }]);
